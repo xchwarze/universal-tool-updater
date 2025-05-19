4e711c5b3f15cfd57ae11fc12ad47a9dce12143d
DSR! <xchwarze@gmail.com>
2025-05-19 12:38:15 -0300

    Refactor Scraper. Add new logic.

--- src/universal_updater/Helpers.py
+++ src/universal_updater/Helpers.py
@@ -1,5 +1,6 @@
 import pathlib
 import shutil
+from urllib.parse import urlparse
 
 
 class Helpers:
@@ -8,11 +9,17 @@ class Helpers:
     def cleanup_folder(path):
         """
         Clean up a folder by deleting all its contents.
-
-        :param path: Path to the folder to be cleaned
         """
         for file in pathlib.Path(path).iterdir():
             if file.is_dir():
                 shutil.rmtree(file)
             else:
                 file.unlink()
+
+    @staticmethod
+    def is_valid_url(url: str) -> bool:
+        """
+        Return True if URL has a valid HTTP/S scheme and network location.
+        """
+        parts = urlparse(url)
+        return parts.scheme in ("http", "https") and bool(parts.netloc)
